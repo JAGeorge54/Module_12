b3ee518b02b83628aaf034c8e4db8e125373d7c5
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-15 16:19:36 -0400

    added code

--- Self-study Exercise/introduction-to-jsx-components/element.jsx
+++ Self-study Exercise/introduction-to-jsx-components/element.jsx
@@ -1,4 +1,16 @@
-const name = 'Mary';
-const element = <h1> Hello {name} </h1>;
+const Hello = props => {
+    let color = props.action();
+    return <h1 style = {{backgroundColor: color}}>hello {props.name} </h1>;
+};
+const getRandomColor = () => {
+    const palet = ['red', 'green', 'blue'];
+    let color = palet[Math.floor(Math.random() * 3)];
+    console.log(color);
+    return color;
+};
 
-ReactDOM.render(element, document.getElementById('root'));
+
+
+
+ReactDOM.render(
+<Hello action = {getRandomColor} name = 'Lisa' />, document.getElementById('root'));
--- Self-study Exercise/introduction-to-jsx-render-element/getData0.jsx
+++ Self-study Exercise/introduction-to-jsx-render-element/getData0.jsx
@@ -1,7 +1,26 @@
+// const App = () => {
+//   let product = { name: 'Pear', cost: 2, inStock: 7 };
+// let {name, inStock} = product;
+// let item = {name, inStock};
+//   const handler = () => alert(`Name : ${item.name} Instock: ${item.inStock}`);
+//   return <button onClick={handler}>Click Me</button>;
+// };
+
+// //Destructure an object
+
+// //---------------
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+//======================================
+
 const App = () => {
-  const handler = () => alert('hello');
-  return <button onClick={handler}>Click Me</button>;
+  
+  const handler = () => alert(`hello`);
+  return <MyButton onClick = {handler}> </MyButton>
 };
+const MyButton = (props) => {
+  return <button onClick={props.onClick}> Click Me </button>;
+}
 
 //Destructure an object
 
